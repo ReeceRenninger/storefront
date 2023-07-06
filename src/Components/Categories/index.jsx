c1c151e3dec32d7bcbd4a8de6ce56ddca0aec662
ReeceRenninger <renningerreece@gmail.com>
2023-07-06 16:34:31 -0700

    working on store reducer

--- src/Components/Categories/index.jsx
+++ src/Components/Categories/index.jsx
@@ -19,12 +19,12 @@ function Categories () {
             <h1>Browse Our Categories</h1>
             {
               categories.map((category, index) => (
-                <article key={`category-${index}`}>
+                <article key={`categories-${index}`}>
                   <h2 onClick={() => categoryHandler(category)}>{category.displayName}</h2>
                 </article>
               ))
             }
-            <p>NEED 2 LINKS FOR ELECTRONICS AND FOODS CATEGORY REFERENCE LAB IMAGE</p>
+            <p>NEED 3 LINKS FOR ELECTRONICS, FOOD, AND CLOTHES CATEGORIES REFERENCE LAB IMAGE</p>
         </>
     )
 }
