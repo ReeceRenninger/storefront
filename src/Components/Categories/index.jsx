6cf4caa9d2b214f6c43687654ffbca7ce7a85e9a
ReeceRenninger <renningerreece@gmail.com>
2023-07-06 16:17:00 -0700

    trying to set up store and categories component

--- src/Components/Categories/index.jsx
+++ src/Components/Categories/index.jsx
@@ -1,11 +1,31 @@
+import { useDispatch, useSelector } from 'react-redux';
+import changeCategory from '../../store/categories';
+
 //TODO: Set up navbar with routes to each category of food and electronics
 //TODO: Have large Text that signifies the active category at center of page
 function Categories () {
+
+  const { categories } = useSelector((state) => state.categories);
+  console.log('categories', categories);
+
+  const dispatch = useDispatch();
+
+  const categoryHandler = (category) => {  
+    dispatch(changeCategory(category));
+  };
+
     return (
-        <div>
+        <>
             <h1>Browse Our Categories</h1>
+            {
+              categories.map((category, index) => (
+                <article key={`category-${index}`}>
+                  <h2 onClick={() => categoryHandler(category)}>{category.displayName}</h2>
+                </article>
+              ))
+            }
             <p>NEED 2 LINKS FOR ELECTRONICS AND FOODS CATEGORY REFERENCE LAB IMAGE</p>
-        </div>
+        </>
     )
 }
 
--- src/store/categories/index.js
+++ src/store/categories/index.js
@@ -4,4 +4,35 @@ let initialState = {
     { name: 'food', displayName: 'Food' },
     { name: 'clothing', displayName: 'Clothing' },
   ],
+  activeCategory: '',
 };
+
+function categoriesReducer(state = initialState, action) {
+  switch(action.type){
+    case 'CHANGE_CATEGORY':
+      return {
+        ...state,
+        categories: state.categories.map(category => {
+          if(category.name === action.payload.name){
+            return {
+              name: category.name,
+              displayName: category.displayName,
+              activeCategory: category.name, // TODO: check if this works
+            }
+          }
+          return category;
+      }),
+    }
+    default: //!! IS THIS NEEDED?
+      return state;
+  }
+}
+
+export const changeCategory = (category) => {
+  return {
+    type: 'CHANGE_CATEGORY',
+    payload: category,
+  }
+}
+
+export default categoriesReducer;
