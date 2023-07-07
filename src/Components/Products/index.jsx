e05aadb522ba357171fce5bf11d9e4a0bdcecc1e
ReeceRenninger <renningerreece@gmail.com>
2023-07-07 14:22:06 -0700

    styling and adding thunk

--- src/Components/Products/index.jsx
+++ src/Components/Products/index.jsx
@@ -1,49 +1,50 @@
 import { Button, Card, CardActions, CardContent, CardMedia, Grid, Typography } from '@mui/material';
-// import { When } from 'react-if';
+import { When } from 'react-if';
 import { useSelector } from 'react-redux';
 
 import '../../../App.css'
 
 function Products() {
 
-  const { activeCategory, products } = useSelector((state) => state.products);
+  const { products } = useSelector((state) => state.products);
+  const { activeCategory } = useSelector((state) => state.categories);
   console.log('products', products);
 
   return (
-    products.map((product, index) => (
-      <>
-          <Grid className='productCards' spacing={3}  key={`product-${index}`}>
-        {
-        //  <When condition={activeCategory}>
-        <Grid item xs={12} sm={6} md={6} lg={4}>
-              <Card  variant="outlined" sx={{ maxWidth: 345 }}>
-                <CardMedia id="img-container"
-                  sx={{ height: 140 }}
-                  image={`https://source.unsplash.com/random/?${product.name}}`}
-                  title="random image"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    Name: {product.name}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    Price: {product.price} doll hairs
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    In-Stock: {product.inStock}
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  <Button size="small">Add to Cart</Button>
-                  <Button size="small">View Details</Button>
-                </CardActions>
-              </Card >
-            </Grid>
-        // </When>
-      }
-      </Grid>
+    <When condition={activeCategory}>
+      {products.map((product, index) => (
+        <>
+          <Grid className='productCards' spacing={3} key={`product-${index}`}>
+            {
+              <Grid item xs={12} sm={6} md={6} lg={4}>
+                <Card variant="outlined" sx={{ maxWidth: 345 }}>
+                  <CardMedia id="img-container"
+                    sx={{ height: 140 }}
+                    image={`https://source.unsplash.com/random/?${product.name}}`}
+                    title="random image"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      Name: {product.name}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Price: {product.price} doll hairs
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      In-Stock: {product.inStock}
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small">Add to Cart</Button>
+                    <Button size="small">View Details</Button>
+                  </CardActions>
+                </Card >
+              </Grid>
+            }
+          </Grid>
         </>
-    ))
+      ))}
+    </When>
   )
 }
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 // redux store hub
-import { legacy_createStore as createStore, combineReducers } from "redux";
+import { legacy_createStore as createStore, combineReducers, applyMiddleware } from "redux";
 import { composeWithDevTools } from "@redux-devtools/extension";
-
+import thunk from "redux-thunk";
 import categoriesReducer from './categories';
 import productsReducer from './products';
 
@@ -11,9 +11,7 @@ let reducers = combineReducers({
 });
 
 const store = () => {
-  return createStore(reducers, composeWithDevTools());
+  return createStore(reducers, composeWithDevTools(applyMiddleware(thunk)));
 }
 
 export default store();
-// const map = new Map();
-// map.set('categories', categoriesReducer);
